7e48da371de5890fa4cc808a0e5d82fded6b445a
osman_alsmanie <73757409+osmankiv@users.noreply.github.com>
2024-10-24 21:09:13 +0200

    Add files via upload

---
+++ Encryption of the textual chains,but using caear's algorithm.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	cout <<"******************************************************\n* Enter Your TxT *** wlcom In Engribted system******\n****";
+	string text;
+	cin>>text;
+	cout <<"******************************************************\n* Enter Your key ********************************\n*\n******";
+	int key;
+	cin>>key;
+	
+	int a=0,b=1,c=2,d=3,e=4,f=5,g=6,h=7,i=8,j=9,k=10,l=11,n=12,m=13,o=14,p=15,q=16,r=17,s=18,t=19,u=20,v=21,w=22,x=23,y=24,z=25;
+//	cout<<;
+	return 0;
+}
